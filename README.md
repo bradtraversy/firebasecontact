4609782d42dec578b582c8b1d794f265a5f3843c
Brad Traversy <me@bradtraversy.com>
2017-08-20 08:45:11 -0400

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,3 @@
+#Firebase Contact Form
+
+Mobile first, responsive contact from that sends data to a firebase database
---
+++ main.js
@@ -0,0 +1,59 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCIm8gDGoVacLoqagK5rFcFKKr8DDDfh2Q",
+  authDomain: "contactform-6a0b0.firebaseapp.com",
+  databaseURL: "https://contactform-6a0b0.firebaseio.com",
+  projectId: "contactform-6a0b0",
+  storageBucket: "contactform-6a0b0.appspot.com",
+  messagingSenderId: "566981475490"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e){
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var company = getInputVal('company');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, company, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, company, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    company:company,
+    email:email,
+    phone:phone,
+    message:message
+  });
+}
